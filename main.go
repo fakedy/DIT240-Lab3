8bb5573d908696b8148a4a17e0e5c9ecfe95ad85
fakedy <34602297+fakedy@users.noreply.github.com>
2025-12-04 13:41:19 +0100

    Fixed start of args

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
+package main
 
+import (
+	"flag" // https://pkg.go.dev/flag
+)
 
+func main() {
+	// somehow take in arguments
+	var (
+		IP       string
+		port     int
+		joinIP   string
+		joinPort int
+		ts       int
+		tff      int
+		tcp      int
+		r        int
+	)
 
+	flag.StringVar(&IP, "a", "127.0.0.1", "Chord IP Address")
+	flag.IntVar(&port, "p", 8080, "Chord Port")
+	flag.StringVar(&joinIP, "ja", "", "IP Address to join")
+	flag.IntVar(&joinPort, "jp", 0, "Port to join")
+	flag.IntVar(&ts, "ts", 3000, "Stabilization Time (ms)")
+	flag.IntVar(&tff, "tff", 1000, "Fix fingers time (ms)")
+	flag.IntVar(&tcp, "tcp", 3000, "Check predecessor time (ms)")
+	flag.IntVar(&r, "r", 4, "number of successors maintained by chord client")
 
+	flag.Parse()
 
-func main {
-	
 }
